54410a7b79e2411a3da0dd6b5f88b9b1098e8a61
husam287 <husam287@hotmai.com>
2024-03-12 10:54:00 +0200

    fix: laggy home carousel

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import ScreenWrapper from "@/components/templates/ScreenWrapper";
 import HomeProductSlider from "@/components/organisms/HomeProductSlider";
@@ -6,6 +7,7 @@ import Paper from "@/components/templates/Paper";
 import ScrollViewWithoutBar from "@/components/templates/ScrollViewWithoutBar";
 import AdvertiseSliderBanner from "@/components/organisms/AdvertiseSliderBanner";
 import useWishlistProducts from "@/hooks/useWishlistProducts";
+import GLOBAL_STYLES from "@/constants/GlobalStyles";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -25,34 +27,31 @@ export default function HomeScreen() {
 
   return (
     <ScreenWrapper>
-      <ScrollViewWithoutBar
-        alwaysBounceHorizontal={false}
-        alwaysBounceVertical={false}
-        bounces={false}
-        overScrollMode="never"
-      >
-        <Paper>
-          <AdvertiseSliderBanner />
-        </Paper>
+      <GestureHandlerRootView style={GLOBAL_STYLES.fullSize}>
+        <ScrollViewWithoutBar>
+          <Paper>
+            <AdvertiseSliderBanner />
+          </Paper>
 
-        <Paper>
-          <HomeProductSlider
-            listTitle="Featured Products"
-            products={featuredProducts?.products || []}
-            listNavAction={onGoToFeaturedProduct}
-            isLoading={isFeaturedProductsLoading}
-          />
-        </Paper>
+          <Paper>
+            <HomeProductSlider
+              listTitle="Featured Products"
+              products={featuredProducts?.products || []}
+              listNavAction={onGoToFeaturedProduct}
+              isLoading={isFeaturedProductsLoading}
+            />
+          </Paper>
 
-        <Paper>
-          <HomeProductSlider
-            listTitle="Liked Products"
-            products={wishlistProducts || []}
-            listNavAction={onGoToWishlist}
-            isLoading={isFeaturedProductsLoading}
-          />
-        </Paper>
-      </ScrollViewWithoutBar>
+          <Paper>
+            <HomeProductSlider
+              listTitle="Liked Products"
+              products={wishlistProducts || []}
+              listNavAction={onGoToWishlist}
+              isLoading={isFeaturedProductsLoading}
+            />
+          </Paper>
+        </ScrollViewWithoutBar>
+      </GestureHandlerRootView>
     </ScreenWrapper>
   );
 }
